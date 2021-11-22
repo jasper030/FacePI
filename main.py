86d8b30724d1c9022176fb1d7c3398095df2e3e8
jasper030 <67250468+jasper030@users.noreply.github.com>
2021-11-22 11:51:56 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import json, fire
+
+class FacePI:
+    
+  def readConfig(self):
+
+    with open("Config.json", "r", encoding="utf-8") as f:
+        config = json.load(f)
+    return config
+
+  def writeConfig(self, config):
+    with open("Config.json", "w", encoding="utf-8") as f:
+      json.dump(config, f)
+
+  def setConfig(self):
+      config = self.readConfig()
+      print("The '[]' after th value is the default value, press ENTER if you don't want to modify it.")
+      api_key = input(f'Please enter the API_KEY[{config["api_key"]}]: ')
+      if api_key: config["api_key"] = api_key
+      title = input(f'Please enter the TITLE[{config["title"]}]: ')
+      if title: config["title"] = title
+      self.writeConfig(config)
+
+    
+  def showConfig(self):
+      print(self.readConfig())
+
+
+if __name__ == '__main__':
+    fire.Fire(FacePI)
