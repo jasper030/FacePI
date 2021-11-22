41ecf1948f7d3dd17c7aeb32296df2b86d04976a
jasper030 <67250468+jasper030@users.noreply.github.com>
2021-11-22 11:53:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json, fire
 class FacePI:
     
   def readConfig(self):
-
     with open("Config.json", "r", encoding="utf-8") as f:
         config = json.load(f)
     return config
@@ -13,17 +12,16 @@ class FacePI:
       json.dump(config, f)
 
   def setConfig(self):
-      config = self.readConfig()
-      print("The '[]' after th value is the default value, press ENTER if you don't want to modify it.")
-      api_key = input(f'Please enter the API_KEY[{config["api_key"]}]: ')
-      if api_key: config["api_key"] = api_key
-      title = input(f'Please enter the TITLE[{config["title"]}]: ')
-      if title: config["title"] = title
-      self.writeConfig(config)
-
+    config = self.readConfig()
+    print("The '[]' after th value is the default value, press ENTER if you don't want to modify it.")
+    api_key = input(f'Please enter the API_KEY[{config["api_key"]}]: ')
+    if api_key: config["api_key"] = api_key
+    title = input(f'Please enter the TITLE[{config["title"]}]: ')
+    if title: config["title"] = title
+    self.writeConfig(config)
     
   def showConfig(self):
-      print(self.readConfig())
+    print(self.readConfig())
 
 
 if __name__ == '__main__':
