f16f31ac7f02baa8317c776578e3580f781b885a
Japser <jasperican@gmail.com>
2021-12-13 11:59:38 +0800

    add classes pachage(FaceAPI hasn't revised yet.)

---
+++ classes/ClassConfig.py
@@ -0,0 +1,27 @@
+import json, os
+
+class Config:
+    def __init__(self) -> None:
+        basepath = os.path.dirname(os.path.realpath(__file__))
+        self.Configpath = os.path.join(basepath, '../Config.json')
+
+    def readConfig(self):
+        with open(self.Configpath, "r", encoding="utf-8") as f:
+            config = json.load(f)
+        return config
+
+    def writeConfig(self, config):
+        with open(self.Configpath, "w", encoding="utf-8") as f:
+            json.dump(config, f)
+
+    def setConfig(self):
+        config = self.readConfig()
+        print("The '[]' after th value is the default value, press ENTER if you don't want to modify it.")
+        api_key = input(f'Please enter the API_KEY, current -> [{config["api_key"]}]: ')
+        if api_key: config["api_key"] = api_key
+        title = input(f'Please enter the TITLE, current -> [{config["title"]}]: ')
+        if title: config["title"] = title
+        self.writeConfig(config)
+    
+    def showConfig(self):
+        print(self.readConfig())
